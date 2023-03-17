e828dc9449732c3319b733dcdc58402e924c9625
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-03-17 13:03:54 +0100

    ff

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,23 +5,27 @@ const body = document.getElementsByTagName('body')[0];
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
 let timerId;
 stop.disabled = true;
 
-  start.addEventListener("click", () => {
-    stop.disabled=false;
-    start.disabled=true;
-    startInterval()
-  });
-  function startInterval() {
-    timerId = setInterval(() => {
-      const color = getRandomHexColor();
-      body.style.backgroundColor = color;
-    }, 1000);
-  }
-    function stopInterval() {
-      clearInterval(timerId);
-    }
+start.addEventListener("click", () => {
+  stop.disabled=false;
+  start.disabled=true;
+  startInterval()
+});
+
+function startInterval() {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopInterval() {
+  clearInterval(timerId);
+}
+
 stop.addEventListener('click', () => {
   stopInterval();
   stop.disabled = true;
