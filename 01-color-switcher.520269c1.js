2b462bbf037507b6c551126edf9dd20ceea1a8fd
YurionStyle99 <107572718+YurionStyle99@users.noreply.github.com>
2023-03-17 11:50:04 +0000

    Deploying to gh-pages from @ YurionStyle99/goit-js-hw-09@7459d072638df77cf93cc2212d089d04f0d9cc7e 🚀

---
+++ 01-color-switcher.520269c1.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.getElementsByTagName("body")[0];t.disabled=!0,e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));a.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(function(){clearInterval(),t.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.520269c1.js.map
