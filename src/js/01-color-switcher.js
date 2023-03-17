90476d4703c2aa968a19e89c66c8f3769945d302
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-03-17 13:22:13 +0100

    final fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,25 @@
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
 let intervalId;
-const body = document.body;
-const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('[data-stop]');
+stopBtn.disabled = true;
 
 function startColorChange() {
-  startBtn.disabled = true; // деактивируем кнопку "Start"
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
   intervalId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
 function stopColorChange() {
-  clearInterval(intervalId); // останавливаем изменение цвета фона
-  startBtn.disabled = false; // активируем кнопку "Start"
+  clearInterval(intervalId);
+  startBtn.disabled = false;
 }
 
 startBtn.addEventListener('click', startColorChange);
