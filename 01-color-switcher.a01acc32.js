51b2a185711bcb26695cad75a73bbdc65668ba48
YurionStyle99 <107572718+YurionStyle99@users.noreply.github.com>
2023-03-17 11:58:09 +0000

    Deploying to gh-pages from @ YurionStyle99/goit-js-hw-09@012b704968594a3bdecdf368d6c8a1d432d87d1b 🚀

--- 01-color-switcher.a01acc32.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.getElementsByTagName("body")[0];t.disabled=!0,e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;d.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(()=>{clearInterval(),t.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.a01acc32.js.map
